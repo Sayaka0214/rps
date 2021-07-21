eb78cfedeba1ff5779c4247d1cdac6581f9198dd
SayakaFujino <sayaka.19920214@gmail.com>
2021-07-21 03:04:50 +0000

    2nd time commit

--- rps0719.rb
+++ rps0719.rb
@@ -1,10 +1,14 @@
-# class Jankengame
+class Jankengame
+    
+    def initialize 
+        @game_result = ""
+    end   
+    
     puts "じゃんけん"
     def janken
-        @game_result = ""
         puts "0(グー)1(チョキ)2(パー)3(戦わない)"
         player_hand = gets.to_i
-        program_hand = 1
+        program_hand = rand(3)
         jankens = ["グー", "チョキ", "パー"]
         puts "ホイ！"
         puts "---------------"
@@ -13,39 +17,30 @@
 
         win = (player_hand == 0 && program_hand == 1) || (player_hand == 1 && program_hand == 2) || (player_hand == 2 && program_hand == 0)
         draw = player_hand == program_hand
-        lose = !(win && draw)
+        # lose = !(win && draw)
+        
         if draw
             puts "---------------"
             puts "あいこで"
-            # return true
             @game_result = "draw"
-            janken
         elsif win
             puts "▶︎あなたの勝ち"
             puts "---------------"
             @game_result = "win"
-            # return false
-        else lose
+        else 
             puts "▶︎あなたの負け"
             puts "---------------"
             @game_result = "lose"
-            # return false
         end  
-    end 
-    
-    janken
     
     if @game_result == "draw"
         janken
-    end
-
-    if @game_result == "win"
+    elsif @game_result == "win"
         puts "あっち向いて"
         puts "0(上) 1(下) 2(右) 3(左)"
         player_finger = gets.to_i
-        program_face = 1
+        program_face = rand(4)
         directions = ["上", "下", "右", "左"]
-
         puts "ホイ"
         puts "---------------"
         if player_finger == program_face
@@ -58,16 +53,13 @@
             puts "相手  ：#{directions[program_face]}"
             puts "▶︎残念！"
             puts "---------------"
-        end
-    end     
-
-    if @game_result == "lose"
-        puts "プログラム：あっち向いて"
-        puts "あなた　　：0(上) 1(下) 2(右) 3(左)"
+        end        
+    elsif @game_result == "lose"
+        puts "相手　　：あっち向いて"
+        puts "あなた  ：0(上) 1(下) 2(右) 3(左)"
         player_face= gets.to_i
         program_finger = 1
         directions = ["上", "下", "右", "左"]
-
         puts "ホイ"
         puts "---------------"
         if player_face == program_finger
@@ -80,9 +72,10 @@
             puts "相手  ：#{directions[program_finger]}"
             puts "▶︎勝敗つかず"
             puts "---------------"
-        end
+        end    
+    end
     end
 
-# end
-# jankengame = Jankengame.new
-# jankengame.janken
+end
+jankengame = Jankengame.new
+jankengame.janken
